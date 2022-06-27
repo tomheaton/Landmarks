10afabf75d3078d142f64e404757548e48eb6242
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-06-27 12:16:23 +0100

    Building Lists and Navigation

---
+++ Shared/Views/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkList.swift
+//  TurtleRock
+//
+//  Created by Tom Heaton on 27/06/2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
