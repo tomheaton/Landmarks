243bc64e819a25b1a0098e3e2026d3ef19e84a73
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-06-27 10:36:58 +0100

    Initial Commit

---
+++ Shared/TurtleRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurtleRockApp.swift
+//  Shared
+//
+//  Created by Tom Heaton on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TurtleRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
