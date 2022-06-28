664a73d64e98697dd47512b043ad3153b4dbed75
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-06-28 13:08:03 +0100

    Refactor project to Landmarks

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tom Heaton on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
+
